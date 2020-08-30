df01662e148c9388ba6725cfa7b20e24a5c0fb0e
hihi-haha-hoho <70221878+hihi-haha-hoho@users.noreply.github.com>
2020-08-30 16:26:54 +0700

    Add files via upload

---
+++ Day2/homework.js
@@ -0,0 +1,16 @@
+// 1.	let vs var vs const
+// What are var and const in JavaScript?
+// var and const are used to declare variables in JS.
+
+// What are the differences between let and var?
+//The difference between let and var is their scope. Variables declared with both var and let can be reassigned.
+//var can have global and function scope, but not block scope.
+//let can have block and function scope, but not global scope.
+
+// What are the differences between let and const?
+//The difference between let and const is assignability. Variables declared with const cannot be reassigned, while those declared with let can be reassigned.
+
+// What to use in what cases?
+//Use let when you want to change the value of the variables. Use const when you know for sure the variables are unchanged.
+
+//2.	Boolean
