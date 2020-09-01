17f21590e588c98d57b74ad807d34e33fefe770f
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2020-09-02 01:33:16 +0700

    class-day3-final

--- Day3/class.js
+++ Day3/class.js
@@ -40,22 +40,18 @@ let books = [
 {
   let bill = [124, 48, 268];
   let tipPercent;
-  let tips;
-  let total;
   let tipEachBill = [];
   let billAndTips = [];
   for (let i = 0; i < bill.length; i++) {
-    if (bill[i] < 50) {
+    if (bill[i] <= 50) {
       tipPercent = 0.2;
-    } else if (bill[i] > 50 && bill[i] < 200) {
+    } else if (bill[i] > 50 && bill[i] <= 200) {
       tipPercent = 0.15;
     } else {
       tipPercent = 0.1;
     }
-    tips = bill[i] * tipPercent;
-    total = bill[i] + tips;
-    tipEachBill.push(tips);
-    billAndTips.push(total);
+    tipEachBill[i] = bill[i] * tipPercent;
+    billAndTips[i] = bill[i] + tipEachBill[i];
   }
   console.log(`Amount of tips for each bill: ${tipEachBill}`);
   console.log(`Total amount for each bill: ${billAndTips}`);
