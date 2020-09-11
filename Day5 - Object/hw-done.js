b1d59aac71ec1aa94a5d65a4e17d597ddeb36587
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2020-09-11 18:49:02 +0700

    hw-wip

--- Day5 - Object/hw-done.js
+++ Day5 - Object/hw-done.js
@@ -338,3 +338,159 @@ while (true) {
     }
   }
 }
+
+// Turtle exercise:
+// 7.
+const pos = {
+  x: 200,
+  y: 50,
+};
+penup();
+rt(90);
+fd(pos.x);
+lt(90);
+fd(pos.y);
+pendown();
+
+// 8.
+const square = {
+  x: 100,
+  y: 50,
+  width: 20,
+};
+penup();
+rt(90);
+fd(square.x);
+lt(90);
+fd(square.y);
+pendown();
+for (let i = 0; i < 4; i++) {
+  fd(square.width);
+  rt(90);
+}
+
+// 9.
+const rect = {
+  x: 100,
+  y: 50,
+  width: 20,
+  height: 40,
+};
+penup();
+rt(90);
+fd(rect.x);
+lt(90);
+fd(rect.y);
+pendown();
+for (let i = 0; i < 2; i++) {
+  fd(rect.height);
+  rt(90);
+  fd(rect.width);
+  rt(90);
+}
+
+// 10.
+clear();
+const cmds = [
+  { shape: "square", x: 20, y: 40, width: 50 },
+  { shape: "rect", x: 8, y: 70, width: 12, height: 40 },
+  { shape: "rect", x: 70, y: 70, width: 12, height: 40 },
+  { shape: "rect", x: 10, y: 20, width: 70, height: 20 },
+  { shape: "square", x: 25, y: 40, width: 14 },
+  { shape: "square", x: 33, y: 42, width: 6 },
+  { shape: "square", x: 50, y: 40, width: 14 },
+  { shape: "square", x: 58, y: 42, width: 6 },
+  { shape: "square", x: 40, y: 25, width: 8 },
+  { shape: "square", penWidth: 1, x: 500, y: 500, width: 15 },
+];
+for (let i = 0; i < cmds.length; i++) {
+  let { shape, x, y, width, height } = cmds[i];
+  home();
+  switch (shape) {
+    case "square":
+      penup();
+      rt(90);
+      fd(x);
+      lt(90);
+      fd(y);
+      pendown();
+      for (let i = 0; i < 4; i++) {
+        fd(width);
+        rt(90);
+      }
+      break;
+    case "rect":
+      penup();
+      rt(90);
+      fd(x);
+      lt(90);
+      fd(y);
+      pendown();
+      for (let i = 0; i < 2; i++) {
+        fd(height);
+        rt(90);
+        fd(width);
+        rt(90);
+      }
+      break;
+  }
+}
+
+// 11.
+clear();
+const cmds = [
+  { shape: "circle", x: 0, y: -20, radius: 60 },
+  { shape: "circle", x: 0, y: 80, radius: 40 },
+  { shape: "circle", x: -75, y: 10, radius: 20 },
+  { shape: "circle", x: 75, y: 10, radius: 20 },
+  { shape: "square", x: -65, y: -100, width: 30 },
+  { shape: "square", x: 38, y: -100, width: 30 },
+  { shape: "rect", x: -30, y: 80, width: 20, height: 4 },
+  { shape: "rect", x: 10, y: 80, width: 20, height: 4 },
+  { shape: "rect", x: -10, y: 50, width: 20, height: 6 },
+  { shape: "square", x: 500, y: 500, width: 20 },
+];
+for (let i = 0; i < cmds.length; i++) {
+  let { shape, x, y, width, height, radius } = cmds[i];
+  home();
+  switch (shape) {
+    case "square":
+      penup();
+      rt(90);
+      fd(x);
+      lt(90);
+      fd(y);
+      pendown();
+      for (let i = 0; i < 4; i++) {
+        fd(width);
+        rt(90);
+      }
+      break;
+    case "rect":
+      penup();
+      rt(90);
+      fd(x);
+      lt(90);
+      fd(y);
+      pendown();
+      for (let i = 0; i < 2; i++) {
+        fd(height);
+        rt(90);
+        fd(width);
+        rt(90);
+      }
+      break;
+    case "circle":
+      penup();
+      rt(90);
+      fd(x);
+      lt(90);
+      fd(y);
+      pendown();
+      for (let i = 0; i < 360; i++) {
+        fd(radius / 57);
+        rt(1);
+      }
+      break;
+  }
+}
