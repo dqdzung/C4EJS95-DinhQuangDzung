88e6f16be0386661e60fb7214f57fd2ed5b39e27
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2020-09-11 19:56:05 +0700

    hw-done

--- Day5 - Object/hw-done.js
+++ Day5 - Object/hw-done.js
@@ -88,15 +88,11 @@ for (let i = 0; i < length; i++) {
 
 // 4. Dictionary
 let myDictionary = {
-  debug:
-    "The process of figuring out why your program has a certain error and how to fix it",
-  done:
-    "When your task is complete, the only thing you have to do is to wait for users to use it (no additional codes or actions needed)",
-  defect: "The formal word for 'error'",
-  pm:
-    "The short version of Project Manager, the person in charge of the final result of a project",
-  "ui/ux":
-    "UI means User Interface, UX mean User Experience, are the process to define how your products looks and feels",
+  "debug": "The process of figuring out why your program has a certain error and how to fix it",
+  "done": "When your task is complete, the only thing you have to do is to wait for users to use it (no additional codes or actions needed)",
+  "defect": "The formal word for 'error'",
+  "pm": "The short version of Project Manager, the person in charge of the final result of a project",
+  "ui/ux": "UI means User Interface, UX mean User Experience, are the process to define how your products looks and feels",
 };
 while (true) {
   const userWord = prompt(
