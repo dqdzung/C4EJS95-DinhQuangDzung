b1fd21cb8c278bffc3c8d22168ea71a75b526a91
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2020-09-22 23:14:21 +0700

    class

--- Day7 - Function (Sep 22th)/class.js
+++ Day7 - Function (Sep 22th)/class.js
@@ -24,15 +24,15 @@ function evenNum(array) {
   }
   return evenArr;
 }
+console.log(test(arr,evenNum));
 
-
-// arrMax = (array) => {
-//   let max = array[0];
-//   for (let i = 1; i < array.length; i++) {
-//     if (array[i] > max) {
-//       max = array[i];
-//     }
-//   }
-//   return max;
-// };
-// console.log("Max: ", arrMax(arr));
+arrMax = (array) => {
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+};
+console.log("Max: ", arrMax(arr));
