6f76fe8bec5f8ced565dcc566fe67e7de2eeee54
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2020-10-06 15:17:05 +0700

    p-wip

--- P/JS/login_reg.js
+++ P/JS/login_reg.js
@@ -1,3 +1,8 @@
+let users = [
+  { id: "test", password: "123456", name: "Test User", role: "user" },
+  { id: "admin", password: "admin", name: "Test Admin", role: "admin" },
+];
+
 const loginDiv = document.getElementById("login-div"),
   regButton = document.getElementById("reg-btn"),
   regID = document.getElementById("reg-id"),
@@ -8,28 +13,39 @@ function loginEvent() {
     loginPassword = document.getElementById("login-pw"),
     loginButton = document.getElementById("login-btn");
   loginButton.addEventListener("click", () => {
-    login(loginID.value, loginPassword.value);
-    loginID.value = loginPassword.value = "";
+    if (loginID.value && loginPassword.value) {
+      login(loginID.value, loginPassword.value);
+      loginID.value = loginPassword.value = "";
+    }
   });
   loginPassword.addEventListener("keyup", (e) => {
-    if (e.key === "Enter") {
-      login(loginID.value, loginPassword.value);
+    if (loginID.value && loginPassword.value) {
+      if (e.key === "Enter") {
+        login(loginID.value, loginPassword.value);
+      }
     }
   });
   loginID.addEventListener("keyup", (e) => {
-    if (e.key === "Enter") {
-      login(loginID.value, loginPassword.value);
+    if (loginID.value && loginPassword.value) {
+      if (e.key === "Enter") {
+        login(loginID.value, loginPassword.value);
+      }
     }
   });
 }
 loginEvent();
 
-async function login(user, password) {
-  const data = await getData();
+function login(user, password) {
   let login = false;
-  for (let i = 0; i < data.length; i++) {
-    if (user === data[i].id && password === data[i].password) {
-      console.log("ID:", user, "PW:", password, `- ${data[i].role}`);
+  for (let i = 0; i < users.length; i++) {
+    if (user === users[i].id && password === users[i].password) {
+      console.log(
+        "Logged In - ID:",
+        user,
+        "PW:",
+        password,
+        `- ${users[i].role}`
+      );
       alert("Login successful!");
       login = true;
       loginDiv.innerHTML = `Welcome back, ${user}! <button onclick="signOut()">Sign out</button>`;
@@ -50,7 +66,28 @@ function signOut() {
   loginEvent();
 }
 
+function signUp(id, password) {
+  let userExist = false;
+  for (let i = 0; i < users.length; i++) {
+    if (id === users[i].id) {
+      userExist = true;
+      console.log(`User "${users[i].id}" already existed`);
+    }
+  }
+  if (userExist) {
+    alert(`User ${id} already existed!`);
+  } else {
+    const newUser = { id: id, password: password, role: "user" };
+    users.push(newUser);
+    console.log("Created user:", id, "PW:", password);
+    alert("Registration completed!");
+  }
+  console.log(users);
+}
+
 regButton.addEventListener("click", () => {
-  postData(regID.value, regPassword.value);
-  regID.value = regPassword.value = "";
-})
+  if (regID.value && regPassword.value) {
+    signUp(regID.value, regPassword.value);
+    regID.value = regPassword.value = "";
+  }
+});
